7ee3799cef5c294c14131818d984cc43bc41a7d1
VShathish <venkatasathish.t@tcs.com>
2025-06-09 18:44:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,13 +29,8 @@ ingredients_list = st.multiselect(
         ,my_dataframe 
         ,max_selections=6
 )
-if ingredients_list: 
-    #is not null: then do everything below this line that is indented. 
-    #st.write(ingredients_list)
-    #st.text(ingredients_list)
-    
+if ingredients_list:   
     ingredients_string = ''
-    
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
